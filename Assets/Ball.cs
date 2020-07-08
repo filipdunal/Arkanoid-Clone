0ef7b1efa63c35607fba899d1e709d9fb93501b4
neo938 <neo938@gmail.com>
2020-07-08 20:47:46 +0200

    Pierwszy dzień

---
+++ Assets/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float maxSpeed = 4f;
+    Rigidbody2D rb;
+    SpriteRenderer ballSprite;
+    Transform paddle;
+
+    [Tooltip("Minimum value of Y velocity. Value below it will be corrected to avoid endless loop of colliding ball between two colliders")]
+    public float minimumYSpeed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ballSprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        paddle = GameObject.Find("Paddle").transform;
+    }
+    private void Update()
+    {
+        ballSprite.transform.rotation = Quaternion.identity;
+
+        if (rb.simulated)
+        {
+            rb.velocity *= 10f;
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+            Debug.Log(rb.velocity.magnitude);
+        }
+        else
+        {
+            transform.position = paddle.position + new Vector3(-0.26f, 0.4f, 0f);
+            if(Input.GetKey(KeyCode.Space))
+            {
+                KickBall();
+            }
+        }
+
+        if(Mathf.Abs(rb.velocity.y)<minimumYSpeed)
+        {
+            rb.velocity = new Vector3(rb.velocity.x,minimumYSpeed*Mathf.Sign(rb.velocity.y));
+        }
+        
+    }
+    void KickBall()
+    {
+        rb.simulated = true;
+        rb.AddForce(Vector3.up*10f);
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speedFactor = 1f;
-    Rigidbody2D rigidbody;
+    Rigidbody2D rb;
     private void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
     public void Update()
     {
-        Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal")*speedFactor, 0f, 0f);
-        rigidbody.velocity=movementVector;
+        if(rb.simulated)
+        {
+            Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal") * speedFactor, 0f, 0f);
+            rb.velocity = movementVector;
+        }
+        
+
+        
     }
     void SetWidth(float width)
     {
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.size = new Vector2(spriteRenderer.size.x, width);
-        CapsuleCollider2D collider = GetComponent<CapsuleCollider2D>();
-        collider.size = new Vector2(collider.size.x+0.45f, collider.size.y);
 
+        //CapsuleCollider2D collider = GetComponent<CapsuleCollider2D>();
+        //collider.size = new Vector2(collider.size.x+0.45f, collider.size.y);
+
+        Transform collider = transform.GetChild(0);
+        collider.localScale = width * Vector3.one;
     }
+
+    
 }
