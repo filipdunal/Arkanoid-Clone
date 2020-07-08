6f98817d93407bfc406df954743671ccf345b8ba
neo938 <neo938@gmail.com>
2020-07-08 16:38:39 +0200

    First commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speedFactor = 1f;
+    Rigidbody2D rigidbody;
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    public void Update()
+    {
+        Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal")*speedFactor, 0f, 0f);
+        rigidbody.velocity=movementVector;
+    }
+    void SetWidth(float width)
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.size = new Vector2(spriteRenderer.size.x, width);
+        CapsuleCollider2D collider = GetComponent<CapsuleCollider2D>();
+        collider.size = new Vector2(collider.size.x+0.45f, collider.size.y);
+
+    }
+}
